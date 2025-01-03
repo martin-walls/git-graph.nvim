42ad324433850f6232d97295752f37b7939cff8e
Martin Walls <martin_w@optimove.com>
2025-01-03 09:13:07 +0000

    trim trailing whitespace

--- lua/git-graph/utils.lua
+++ lua/git-graph/utils.lua
@@ -11,30 +11,39 @@ M.lines = function(str)
     return result
 end
 
+---@param str string
+---@return string
+local function trim_end(str)
+    local res = string.gsub(str, "^(.-)%s*$", "%1")
+    return res
+end
+
 ---@param lines table
 ---@return table
 M.format_graph = function(lines)
     for i, line in ipairs(lines) do
-        local j, _ = string.find(line, "%w%w%w%w%w%w%w - ")
+        -- local j, _ = string.find(line, "%w%w%w%w%w%w%w - ")
+        --
+        -- if j == nil then
+        --     line = string.gsub(line, "%*", "")
+        --     line = string.gsub(line, "|", "")
+        --     line = string.gsub(line, "\\", "╲")
+        --
+        --     lines[i] = line
+        -- else
+        --     local prefix = string.sub(line, 1, j - 2);
+        --     local body = string.sub(line, j - 1);
+        --
+        --     prefix = string.gsub(prefix, "%*", "")
+        --     prefix = string.gsub(prefix, "|", "")
+        --     prefix = string.gsub(prefix, "\\", "╲")
+        --
+        --
+        --
+        --     lines[i] = prefix .. body
+        -- end
 
-        if j == nil then
-            line = string.gsub(line, "%*", "")
-            line = string.gsub(line, "|", "")
-            line = string.gsub(line, "\\", "╲")
-
-            lines[i] = line
-        else
-            local prefix = string.sub(line, 1, j - 2);
-            local body = string.sub(line, j - 1);
-
-            prefix = string.gsub(prefix, "%*", "")
-            prefix = string.gsub(prefix, "|", "")
-            prefix = string.gsub(prefix, "\\", "╲")
-
-
-
-            lines[i] = prefix .. body
-        end
+        lines[i] = trim_end(line)
     end
     return lines
 end
